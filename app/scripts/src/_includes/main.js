1ac9b8d9357275562a37f8a4e8bcb5937a17dbe6
Evgen904 <evgen904@gmail.com>
2019-03-15 16:00:41 +0400

    add animation

--- app/scripts/src/_includes/main.js
+++ app/scripts/src/_includes/main.js
@@ -1,3 +1,26 @@
 $(function(){
 
+
+	function nextStep($this){
+        $this.parents('.js-folder-list').attr('data-active',$this.data('id'));
+        prevStep = $('.js-folder-img > div.active-js').data('step');
+        prevStepSelector = '.js-folder-img > div[data-step="'+prevStep+'"]';
+
+        activeStep = $this.data('id')+1;
+        activeStepSelector = '.js-folder-img > div[data-step="'+activeStep+'"]';
+        $(prevStepSelector).addClass("visible animated flipOutX");
+        $('.js-folder-img > div').removeClass('active-js');
+        $(activeStepSelector).addClass('active-js');
+        $(activeStepSelector).removeClass('flipOutX');
+        $(activeStepSelector).addClass("visible animated flipInX");
+    };
+
+
+    $('.js-folder-list li a').hover(function(){
+        nextStep($(this));
+    },function(){});
+
+
+
+
 });
